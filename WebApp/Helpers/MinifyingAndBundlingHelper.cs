1a464e649dd936a5da286e3bd557e63535fbb0df
mrvshah <viral_ecom@hotmail.com>
2012-05-10 14:57:45 +0100

    Initial commit.

---
+++ WebApp/Helpers/MinifyingAndBundlingHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+
+namespace WebApp.Helpers
+{
+	public static class MinifyingAndBundlingHelper
+	{
+		public static bool IsDebug(this HtmlHelper helper)
+		{
+#if DEBUG
+			return true;
+#else
+			return false;
+#endif
+		}
+
+		public static string GetAssemblyVersion(this HtmlHelper helper)
+		{
+			return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+		}
+	}
+}
